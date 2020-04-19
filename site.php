406d87e266a98cac25c9f401db671dead4f4d666
Ayşe Nur Usta <61902836+ayseenurustaa@users.noreply.github.com>
2020-04-19 19:45:06 +0300

    Add files via upload

---
+++ site.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="tr" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="still.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="genel">
+
+      <div class="menü">
+        <div class="siteadı">
+          AYŞE NUR USTA
+        </div>
+        <div class="mainmenu">
+          <div class="mainmenulist">
+            <a href="#">Makaleler</a>
+          </div>
+          <div class="mainmenulist">
+            <a href="#">Makale Yaz</a>
+          </div>
+          <div class="mainmenulist">
+            <a href="#">Hakkımızda</a>
+          </div>
+          <div class="mainmenulist">
+            <a href="#">İletişim</a>
+          </div>
+        </div>
+        <div class="kullanıcıgirisi">
+          <div class="kullanicigirisib">
+          <b>Kullanıcı Girişi</b>
+          </div>
+          <div class="kullanicigirisitext">
+          <form>
+            Adı Soyadı:<input type="text" name="username">
+            <br>
+            Kullanıcı Adı :<input type="text" name="username">
+            <br>
+            E-postanız:<input type="text" name="username">
+            <br>
+            Şifreniz:<input type="password" name="password">
+          </form>
+          <form>
+            <input type="submit" name="Kayıt ol" value="Kayıt ol">
+          </form>
+          </div>
+          <a href="#">Şifremi Unuttum</a>/<a href="#">Giriş Yap!</a>
+        </div>
+      </div>
+
+      <div class="sayfa">
+        <textarea name="metinkutusu" rows="50" cols="80" wrap="soft" readonly>
+          Bahar Aylarında Okunacak 6 Kitap
+          Bahar ayları yeni bir başlangıcın da sembolüdür. Doğa gibi yaşamın da canlandığı bu aylarda kültürel gelişiminize katkıda bulunmanız ise kendinize yapabileceğiniz en büyük iyiliklerin başında geliyor. Bahar aylarında okunabilecek 6 kitabı sizler için derledik.
+          Ağaca Tüneyen Baron – Italo Calvino
+          Çocuklar ve yetişkinler için uygun bir içeriğe sahip Ağaca Tüneyen Baron, Calvino’nun kaleme aldığı kurmaca bir ütopyadır. Kitap, henüz 12 yaşındayken babasının tavırlarından rahatsız olan bir çocuğun tüm ömrünü ağaçlarda geçirme isteğinı anlatmaktadır. Kitabın kahramanı yeryüzüne bir daha ayak basmamaya kararlıdır ve ömrünün kalan günlerini ağaçta geçirmeye başlar. Italo Calvino’nun kaleme aldığı bu önemli eser bahar aylarında başlayacağınız kitap okuma serüveni için iyi bir başlangıç olabilir.
+          Gülün Adı – Umberto Eco
+          Çok yönlü bir bilim insanı olan Umberto Eco tarafından kaleme alınan Gülün Adı, Hristiyanlık düşüncesini tarihsel boyutlarıyla tartışan bir roman olmakta birlikte ustaca kurulmuş ve sürükleyici bir polisiyedir. Ayrıca çağdaş eserlere uzun ve özgün soluk getiren dünyanın önemli eserleri arasında yer alıyor. Yetişkinler için uygun olan Gülün Adı, bahar aylarında okunabilecek gözde eserler arasında yer alıyor.
+          Dönüşüm – Franz Kafka
+          Alman edebiyatının önde gelen isimlerinden biri olan Franz Kafka’nın kaleme aldığı Dönüşüm, sanayi sonrası Batı toplumunun içine düştüğü ve yabancılaşmayı konu alan bir sistem eleştirisidir. 20.yüzyılın en önemli eserleri arasında sayılan Dönüşüm’ün baş kahramanı ise bir böceğe dönüşen Gregor Samsa’dır. Kafka bu eserinde sosyolojik süreçleri güçlü metaforlarla anlatmayı başarmıştır. Roman kısaca tam bir Alman işi.
+          Denizler Altında Yirmi Bin Fersah – Jules Verne
+          Tanzimat döneminde ilk çevirisi yapılan Jules Verne’nin Denizler Altında Yirmi Bin Fersah eseri Kaptan Nemo’nun denizaltı aracıyla yaptığı yolculuğu ve birçok farklı deniz canlısının yer aldığı bir kurmaca olarak öne çıkıyor. Gizemli bir yolculuğa çıkacağınız bu roman hem çocuklar hem de yetişkinler için uygundur.
+          Morgue Sokağı Cinayetleri – Edgar Allan Poe
+          Bugün okuduğumuz ve izlediğimiz birçok polisiye romanının atası sayılan Morgue Sokağı Cinayetleri, Edgar Allan Poe tarafından kaleme alınan önemli eserlerden biridir. Roman, Paris’teki hayali bir sokak olan Rue Morgue’da işlenen bir cinayeti konu alıyor. Kitap çocuklar ve yetişkinler için okunabilir düzeyde.
+          Sarnıç – Sait Faik Abasıyanık
+        </textarea>
+      </div>
+
+
+
+
+
+
+
+
+    </div>
+  </body>
+</html>
